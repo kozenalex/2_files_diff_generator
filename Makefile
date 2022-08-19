8d7b06c9071028d69929e351a596b9a588bd9488
Aleksey Kosin <akosin@udm.ru>
2022-08-19 11:58:15 +0400

    1 step project 2

--- Makefile
+++ Makefile
@@ -9,6 +9,6 @@ publish:
 package-install:
 	python3 -m pip install --user dist/*.whl
 package-uninstall:
-	python3 -m pip uninstall hexlet_code-0.1.1-py3-none-any.whl
+	python3 -m pip uninstall hexlet_code-0.1.0-py3-none-any.whl
 lint:
 	poetry run flake8 gendiff
--- gendiff/scripts/gen_diff.py
+++ gendiff/scripts/gen_diff.py
@@ -1,6 +1,16 @@
-def main():
-    print('hello world!')
+import argparse
+DESCRIPTION = 'Compares two configuration files and shows a difference.'
 
+
+parser = argparse.ArgumentParser(description=DESCRIPTION)
+parser.add_argument('first_file', type=str, help='first file')
+parser.add_argument('second_file', type=str, help='second file')
+
+
+def main():
+    args = parser.parse_args()
+    print(args)
+    
 
 if __name__ == '__main__':
     main()
