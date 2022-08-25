ce6cfacb45f8e6d9fb424228307adeab4e5f823d
Aleksey Kosin <akosin@udm.ru>
2022-08-25 17:45:48 +0400

    add 1 more test

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -2,11 +2,19 @@ from gendiff import generate_diff
 
 
 def test_generate_diff():
-    test_res = open('./gendiff/tests/fixtures/test1.txt')
-    test_str = ''
-    for line in test_res:
-        test_str += line
+    test1_res = open('./gendiff/tests/fixtures/test1.txt')
+    test2_res = open('./gendiff/tests/fixtures/test2.txt')
+    test1_str = ''
+    test2_str = ''
+    for line in test1_res:
+        test1_str += line
+    for line in test2_res:
+        test2_str += line
     assert generate_diff(
         './gendiff/tests/fixtures/test11.json',
         './gendiff/tests/fixtures/test12.json'
-    ) == test_str
+    ) == test1_str
+    assert generate_diff(
+        './gendiff/tests/fixtures/test21.json',
+        './gendiff/tests/fixtures/test22.json'
+    ) == test2_str
