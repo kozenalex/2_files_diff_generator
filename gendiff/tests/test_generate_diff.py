f5e059fd4fb0dbb60b4d7ff5c1b98fa93aadf33d
Aleksey Kosin <akosin@udm.ru>
2022-08-29 06:50:37 +0400

    starting trees

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -4,12 +4,16 @@ from gendiff import generate_diff
 def test_generate_diff():
     test1_res = open('./gendiff/tests/fixtures/test1.txt')
     test2_res = open('./gendiff/tests/fixtures/test2.txt')
+    test3_res = open('./gendiff/tests/fixtures/test3.txt')
     test1_str = ''
     test2_str = ''
+    test3_str = ''
     for line in test1_res:
         test1_str += line
     for line in test2_res:
         test2_str += line
+    for line in test3_res:
+        test3_str += line
     assert generate_diff(
         './gendiff/tests/fixtures/test11.json',
         './gendiff/tests/fixtures/test12.json'
@@ -22,3 +26,7 @@ def test_generate_diff():
         './gendiff/tests/fixtures/test11.yaml',
         './gendiff/tests/fixtures/test12.yaml'
     ) == test1_str
+    assert generate_diff(
+        './gendiff/tests/fixtures/file1.json',
+        './gendiff/tests/fixtures/file2.json'
+    ) == test3_str
---
+++ gendiff/tree.py
@@ -0,0 +1,41 @@
+def make_leave(name, meta = {}):
+    return {
+        'name': name,
+        'type': 'leave',
+        'meta': meta
+    }
+
+
+def make_node(name, children = []):
+    return {
+        'name': name,
+        'type': 'node',
+        'children': children
+    }
+
+
+def get_name(node):
+    return node['name']
+
+
+def get_meta(node):
+    return node['meta']
+
+
+def get_type(node):
+    return node['type']
+
+
+def is_leave(node):
+    return get_type(node) == 'leave'
+
+
+def is_node(node):
+    return get_type(node) == 'node'
+
+
+def get_children(node):
+    if is_node(node):
+        return node['children']
+    else:
+        return None
